58261977216f02f564bff3b6ebd26e3b849e8706
curatio <yhm0411@naver.com>
2019-05-28 13:53:02 +0900

    impl 검색기능

--- Min'sBookShop/src/main/java/com/bookshop/persistence/GoodsDAOImpl.java
+++ Min'sBookShop/src/main/java/com/bookshop/persistence/GoodsDAOImpl.java
@@ -1,18 +1,25 @@
 package com.bookshop.persistence;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
 
 import com.bookshop.domain.GoodsVO;
 import com.bookshop.domain.ImageFileVO;
-
+@Repository("goodsDAO")
 public class GoodsDAOImpl implements GoodsDAO {
-
+	@Autowired
+	private SqlSession sqlsession;
+	
 	@Override
 	public List<GoodsVO> selectGoodsList(String goodsStatus) throws DataAccessException {
-		// TODO Auto-generated method stub
-		return null;
+		// 메소드 호출 시 전달된 조건으로 도서 검색
+		List goodsList = (ArrayList)sqlsession.selectList("mapper.goods.selectGoodList",goodsStatus);
+		return goodsList;
 	}
 
 	@Override
