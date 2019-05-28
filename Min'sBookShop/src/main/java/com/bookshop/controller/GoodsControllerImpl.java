81e3ff3658a5524dea0e90dfd996c3e0cd24cbe8
curatio <yhm0411@naver.com>
2019-05-28 17:29:51 +0900

    컨트롤러 3개, 서비스 수정

---
+++ Min'sBookShop/src/main/java/com/bookshop/controller/GoodsControllerImpl.java
@@ -0,0 +1,107 @@
+package com.bookshop.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookshop.domain.GoodsVO;
+import com.bookshop.service.GoodsService;
+
+import net.sf.json.JSONObject;
+
+@Controller("goodsController")
+@RequestMapping(value="/goods")
+public class GoodsControllerImpl extends BaseController implements GoodsController {
+	@Autowired
+	GoodsService goodsService;
+	
+	@RequestMapping(value="/goodsDetail.do" ,method = RequestMethod.GET)
+	public ModelAndView goodsDetail(/*조회할 상품 번호 전달*/@RequestParam("goods_id") String goods_id,
+			                       HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String viewName=(String)request.getAttribute("viewName");
+		HttpSession session=request.getSession();
+		//상품 정보 조회 후 Map으로 전달
+		Map goodsMap=goodsService.goodsDetail(goods_id);
+		ModelAndView mav = new ModelAndView(viewName);
+		mav.addObject("goodsMap", goodsMap);
+		// 조회한 상품 정보를 빠른 메뉴에 표시하기 위해 전달
+		GoodsVO goodsVO=(GoodsVO)goodsMap.get("goodsVO");
+		addGoodsInQuick(goods_id,goodsVO,session);
+		return mav;
+	}
+	
+	@RequestMapping(value="/keywordSearch.do",method = RequestMethod.GET,produces = "application/text; charset=utf8")
+	public @ResponseBody String  keywordSearch(@RequestParam("keyword") String keyword,
+			                                  HttpServletRequest request, HttpServletResponse response) throws Exception{
+		response.setContentType("text/html;charset=utf-8");
+		response.setCharacterEncoding("utf-8");
+		//System.out.println(keyword);
+		if(keyword == null || keyword.equals(""))
+		   return null ;
+	
+		keyword = keyword.toUpperCase();
+	    List<String> keywordList =goodsService.keywordSearch(keyword);
+	    
+	 // 최종 완성될 JSONObject 선언(전체)
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("keyword", keywordList);
+		 		
+	    String jsonInfo = jsonObject.toString();
+	   // System.out.println(jsonInfo);
+	    return jsonInfo ;
+	}
+	
+	@RequestMapping(value="/searchGoods.do" ,method = RequestMethod.GET)
+	public ModelAndView searchGoods(@RequestParam("searchWord") String searchWord,
+			                       HttpServletRequest request, HttpServletResponse response) throws Exception{
+		String viewName=(String)request.getAttribute("viewName");
+		List<GoodsVO> goodsList=goodsService.searchGoods(searchWord);
+		ModelAndView mav = new ModelAndView(viewName);
+		mav.addObject("goodsList", goodsList);
+		return mav;
+		
+	}
+	
+	private void addGoodsInQuick(String goods_id,GoodsVO goodsVO,HttpSession session){
+		boolean already_existed=false;
+		List<GoodsVO> quickGoodsList; //최근 본 상품 저장 ArrayList
+		quickGoodsList=(ArrayList<GoodsVO>)session.getAttribute("quickGoodsList");
+		
+		if(quickGoodsList!=null){
+			if(quickGoodsList.size() < 4){ //미리본 상품 리스트에 상품개수가 세개 이하인 경우
+				for(int i=0; i<quickGoodsList.size();i++){
+					GoodsVO _goodsBean=(GoodsVO)quickGoodsList.get(i);
+					if(goods_id.equals(_goodsBean.getGoods_id())){
+						already_existed=true;
+						break;
+					}// 상품 목록을 가져와 이미 존재하는 상품인지 비교하고, 이미 존재할 경우 true, false이면 상품 정보를 목록에 저장
+				}
+				if(already_existed==false){
+					quickGoodsList.add(goodsVO);
+				}
+			}
+			
+		}else{// 최근 본 상품 목록을 세션에 저장
+			quickGoodsList =new ArrayList<GoodsVO>();
+			quickGoodsList.add(goodsVO);
+			
+		}
+		// 최근 본 상품목록을 세션에 전달
+		session.setAttribute("quickGoodsList",quickGoodsList);
+		//최근 본 상품 목록에 저장된 상품 개수를 세션에 전달
+		session.setAttribute("quickGoodsListNum", quickGoodsList.size());
+		
+	}
+}
--- Min'sBookShop/src/main/java/com/bookshop/service/GoodsService.java
+++ Min'sBookShop/src/main/java/com/bookshop/service/GoodsService.java
@@ -7,7 +7,7 @@ import com.bookshop.domain.GoodsVO;
 
 public interface GoodsService {
 	public Map<String,List<GoodsVO>> listGoods() throws Exception;
-	public Map goodsDetail(String_goods_id) throws Exception;
+	public Map goodsDetail(String goods_id) throws Exception;
 	public List<String> keywordSearch(String keyword) throws Exception;
 	public List<GoodsVO> searchGoods(String searchWord) throws Exception;
 	}
